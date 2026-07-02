92923a35af79218ff28045cecf9c546f067b750a
mesutmg <mesutahmettravaci@gmail.com>
2026-07-02 16:58:02 +0300

    vue'd

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,5 @@
 <?php
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\GfGController;
-
-Route::get('laravel', function () {
-    return view('laravel');
-});
-
-Route::get('greeting', function () {
-    return 'Hello World';
-});
-
-Route::get('app', function () {
-    return view('app');
-});
-
-Route::get ('emp/{name?}', function ($name = 'Guest') {
-    echo $name;
-});
-
-Route::get('gfg', [GfGController::class, 'index']);
 
 Route::get('/', function () {
     return view('index');
